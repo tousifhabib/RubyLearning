18cfcfb05c9e1c77a11f5389d089180bc87efcb9
Tousif Habib <thabib@mun.ca>
2022-03-04 11:58:01 -0500

    Init commit

---
+++ Basics/myFirstProgram.rb
@@ -0,0 +1,2 @@
+puts "Tousif Habib" #prints line with newline at the end
+print "Giraffe Academy" #prints line with no new line at the end
---
+++ Basics/mySecondProgram.rb
@@ -0,0 +1,12 @@
+character_name = "Tousif Habib"
+character_age = 35
+character_gpa = 2.918
+ismale = true
+flaws = nil
+
+puts character_name.downcase()
+puts character_name.strip() #removes leading whitespace
+puts character_name.length()
+puts character_name.include? "Habib"
+puts character_name[0]
+puts character_name[2,8]
